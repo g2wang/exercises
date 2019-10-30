3a6608e8058aa78cce7cf6ccc228b3253a58fed8
Guangde Wang <g2wang@yahoo.com>
2019-10-29 20:50:15 -0400

    improved max profit trading example

--- maxprofittrading/MaxProfitTrading.java
+++ maxprofittrading/MaxProfitTrading.java
@@ -1,39 +1,45 @@
+import java.util.ArrayList;
+
 public class MaxProfitTrading {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         // given an array of known prices of a stock over time, find the maximum profit
         // by first buying and then selling
-        //int[] p = new int[] {1, 1, 1, 1, 1, 1, 1, 1};
-        //int[] p = new int[] {1, 1, 24, 1, 1, 25, 25, 17};
-        //int[] p = new int[] {10, 11, 8, 7, 1, 1, 1, 2};
-        int[] p = new int[] {10, 10, 8, 7, 5, 3, 2, 2};
-        int[] result = findMaxProfit(p);
-        System.out.println("maxProfit = " + result[0]);
-        if (result[0] > 0) {
-            System.out.println("iBuy = " + result[1] + "; buying price = " + p[result[1]]);
-            System.out.println("iSell = " + result[2] + "; selling price = " + p[result[2]]);
+        ArrayList<int[]> inputs = new ArrayList<>();
+        inputs.add(new int[] {1, 1, 1, 1, 1, 1, 1, 1}); // no profit
+        inputs.add(new int[] {1, 1, 24, 1, 1, 25, 25, 17}); // max prfit 24
+        inputs.add(new int[] {10, 11, 8, 7, 1, 1, 1, 2}); // max prfit 1
+        inputs.add(new int[] {10, 10, 8, 7, 5, 3, 2, 2}); // no profit
+        for (int[] p : inputs) {
+            int[] result = findMaxProfit(p);
+            System.out.print("prices: ");
+            for (int i = 0; i < p.length; i++) {
+                System.out.printf("%4d", p[i]);
+            }
+            System.out.println("");
+            System.out.print("   day: ");
+            for (int i = 0; i < p.length; i++) {
+                System.out.printf("%4d", i+1);
+            }
+            System.out.printf(" -> maxProfit: %d", result[0]);
+            if (result[0] > 0) {
+                System.out.printf(" (buy at day %d with price %d, sell at day %d with price %d)",
+                        result[1]+1, p[result[1]], result[2]+1, p[result[2]]);
+            } 
+            System.out.println("");
         }
     }
 
     public static int[] findMaxProfit(int[] p) {
-        int maxProfit = 0;
-        int iBuy = 0;
-        int iSell = 0;
-        int iPotential = 0;
+        int maxProfit = 0, iBuy = 0, iSell = 0, iMin = 0;
         for (int i = 1; i < p.length; i++) {
-            int d = p[i] - p[iBuy];
-            int dp = p[i] - p[iPotential];
-            if (d > maxProfit) {
-                maxProfit = d;
-                iSell = i;
-            }
-            if (dp > maxProfit) {
-                iBuy = iPotential;
-                maxProfit = dp;
-                iSell = i;
-            }
-            if (p[i] < p[iPotential]) {
-                iPotential = i;
+            if (p[i] < p[iMin]) {
+                iMin = i;
+            } else {
+                int dp = p[i] - p[iMin];
+                if (dp > maxProfit) {
+                    maxProfit = dp; iBuy = iMin; iSell = i;
+                }
             }
         }
         return new int[] {maxProfit, iBuy, iSell};
