6b86e0bf2fca7dd7c1b1fe61ff58a36422e9ad7d
Guangde Wang <g2wang@yahoo.com>
2020-10-19 19:22:02 -0400

    added leet code 223

--- leetcode233/CountDigitOne.java
+++ leetcode233/CountDigitOne.java
@@ -7,7 +7,7 @@ Given an integer n, count the total number of digit 1 appearing in all non-negat
 Example:
 
 Input: 13
-Output: 6 
+Output: 6
 Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.*
  */
 
@@ -16,36 +16,37 @@ public class CountDigitOne {
     public static void main(String[] args) {
         int n = 20;
         CountDigitOne cd1 = new CountDigitOne();
-        int ans = cd1.countDigitOne(n); 
+        int ans = cd1.countDigitOne(n);
         System.out.printf("n: %d -> ans: %d%n", n, ans);
     }
 
     /**
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Number of Digit One.
-     * Memory Usage: 35.9 MB, less than 21.29% of Java online submissions for Number of Digit One.
+     * Memory Usage: 35.6 MB, less than 21.29% of Java online submissions for Number of Digit One.
      */
     public int countDigitOne(int n) {
         if (n < 1) return 0;
-        char[] ca = String.valueOf(n).toCharArray();
+        int len = String.valueOf(n).length();
         int ans = 0;
-        for (int i = ca.length - 1; i >= 0; i--) {
-            ans += count(ca, i);
+        for (int i = len - 1; i >= 0; i--) {
+            ans += count(n, i, len);
         }
         return ans;
     }
 
-    private int count(char[] ca, int i) {
-        int d = ca[i] - '0';
+    private int count(int n, int i, int len) {
         int count = 0;
-        int prefixNumber = 0;
-        for (int j = i-1, f = 1; j >= 0; j--, f *= 10) {
-            prefixNumber = (ca[j] - '0') * f + prefixNumber;
-        }
-        int suffixMax = 1;
+        int prefixNumber = n;
         int suffixNumber = 0;
-        for (int j = i+1; j < ca.length; j++) {
-            suffixNumber = suffixNumber * 10 + (ca[j] - '0');
-            suffixMax *= 10;
+        int suffixMax = 1;
+        int d = 0;
+        for (int j = len-1, f = 1; j >= i; j--, f *= 10) {
+            d = prefixNumber%10;
+            prefixNumber = prefixNumber/10;
+            if (j > i) {
+                suffixNumber += f*d;
+                suffixMax *= 10;
+            }
         }
 
         count += prefixNumber * suffixMax;
@@ -58,4 +59,5 @@ public class CountDigitOne {
 
         return count;
     }
+
 }
