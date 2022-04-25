c496ae0f8cd17f29fd5ccdaeef8b158b32af4935
Guangde Wang <g2wang@gmail.com>
2022-04-24 23:40:07 -0400

    added alternative solution

---
+++ leetcode128/LongestConsecutive2.java
@@ -0,0 +1,66 @@
+/**
+ 
+128. Longest Consecutive Sequence
+Medium
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+Constraints:
+
+0 <= nums.length <= 1E5
+-1E9 <= nums[i] <= 1E9
+
+ */
+
+import java.util.*;
+
+
+/**
+ * Runtime: 19 ms, faster than 81.10% of Java online submissions for Longest Consecutive Sequence.
+ * Memory Usage: 51.9 MB, less than 92.32% of Java online submissions for Longest Consecutive Sequence.
+ */
+public class LongestConsecutive2 {
+
+    public static void main(String[] args) {
+        LongestConsecutive2 lc = new LongestConsecutive2();
+        int[] nums = new int[]{0,3,7,2,5,8,4,6,0,1};
+        int ans = lc.longestConsecutive(nums) ;
+        System.out.printf("%s%nans: %d%n", Arrays.toString(nums), ans);
+    }
+
+     public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum+1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+}
