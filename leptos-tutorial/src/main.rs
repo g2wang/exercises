687eb907bb6ac06232dc2dc300458e12689cd8f9
Guangde Wang <g2wang@gmail.com>
2024-05-12 12:50:44 -0400

    simple button, counter and progress working

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,22 +1,71 @@
-use leptos::{component, create_signal, mount_to_body, view, IntoView};
+use leptos::*;
 
-fn main() {
-    mount_to_body(|| view! { <App/> })
-}
-
+// The #[component] macro marks a function as a reusable component
+// Components are the building blocks of your user interface
+// They define a reusable unit of behavior
 #[component]
 fn app() -> impl IntoView {
+    // here we create a reactive signal
+    // and get a (getter, setter) pair
+    // signals are the basic unit of change in the framework
+    // we'll talk more about them later
     let (count, set_count) = create_signal(0);
+
+    // the `view` macro is how we define the user interface
+    // it uses an HTML-like format that can accept certain Rust values
     view! {
         <button
-            on:click = move |_| {
-                // on stable, this is set_count.set(3);
-                set_count(3);
-            }
+            // on:click will run whenever the `click` event fires
+            // every event handler is defined as `on:{eventname}`
+
+            // we're able to move `set_count` into the closure
+            // because signals are Copy and 'static
+            on:click = {move |_| {
+                set_count.update(|n| *n += 10);
+            }}
+            // class = ("red", move || x() % 2 == 1)
+            style = "position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style = ("left", move || format!("{}px", count() + 100))
+            style = ("background-color", move || format!("rgb({}, {}, 100)", count(), 100))
+            style = ("max-width", "400px")
+            // Set a CSS variable for stylesheet use
+            style = ("--columns", count)
         >
-            "Click me: "
-            // on stable, this is {move || count.get()}
-            { count }
+            // text nodes in RSX should be wrapped in quotes,
+            // like a normal Rust string
+            "Click to move"
         </button>
+        <p>
+            <strong>"Reactive: "</strong>
+            // you can insert Rust expressions as values in the DOM
+            // by wrapping them in curly braces
+            // if you pass in a function, it will reactively update
+            {move || count.get()}
+        </p>
+        <p>
+            <strong>"Reactive shorthand: "</strong>
+            // signals are functions, so we can remove the wrapping closure
+            {count}
+        </p>
+        <p>
+            <strong>"Not reactive: "</strong>
+            // NOTE: if you write {count()}, this will *not* be reactive
+            // it simply gets the value of count once
+            {count()}
+        </p>
+        <progress
+            max = "50"
+            value = move || count() * 2
+        />
+
     }
+}
+
+// This `main` function is the entry point into the app
+// It just mounts our component to the <body>
+// Because we defined it as `fn App`, we can now use it in a
+// template as <App/>
+fn main() {
+    leptos::mount_to_body(|| view! { <App/> })
 }
