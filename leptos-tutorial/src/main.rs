c579fb43a3c2879a49116edd6453ee4d118fec79
Guangde Wang <g2wang@gmail.com>
2024-05-14 00:07:52 -0400

    more examples

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,5 +1,36 @@
 use leptos::*;
 
+// Composing different components together is how we build
+// user interfaces. Here, we'll define a reusable <ProgressBar/>.
+// You'll see how doc comments can be used to document components
+// and their properties.
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar(
+    // Marks this as an optional prop. It defaults to the specified value.
+    // If the default value is not specified, it will default to the default
+    // value of its type, '0' for this case
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+    // will run the `.into()` on the value passed into the prop.
+    #[prop(into)]
+    // Signal<T> is a wrapper for several reactive types.
+    // It can be helpful in component APIs like this, where we
+    // might want to take any kind of reactive value
+    // How much progress should be displayed.
+    progress: Signal<i32>,
+) -> impl IntoView {
+    view! {
+        <progress
+            max = {max}
+            value = progress
+        />
+        <br/>
+    }
+}
+
 // The #[component] macro marks a function as a reusable component
 // Components are the building blocks of your user interface
 // They define a reusable unit of behavior
@@ -10,7 +41,7 @@ fn app() -> impl IntoView {
     // signals are the basic unit of change in the framework
     // we'll talk more about them later
     let (count, set_count) = create_signal(0);
-
+    let double_count = move || count() * 2;
     // the `view` macro is how we define the user interface
     // it uses an HTML-like format that can accept certain Rust values
     view! {
@@ -56,9 +87,20 @@ fn app() -> impl IntoView {
         </p>
         <progress
             max = "50"
-            value = move || count() * 2
+            value = double_count
         />
-
+        <p>
+            <strong>"double count: "</strong>
+            { double_count }
+        </p>
+        // In and editor with rust-analyzer support, try hovering over `ProgressBar`,
+        // `max`, or `progress` to see the docs we defined above
+        <ProgressBar max = 50 progress = count />
+        // Let's use the default max of 100, which makes this ProgressBar half as fast.
+        <ProgressBar progress = count />
+        // Signal::derive creates a signal wrapper of the derived double_count signal,
+        // which makes this ProgressBar move twice as fast
+        <ProgressBar max = 50 progress = Signal::derive(double_count)/>
     }
 }
 
